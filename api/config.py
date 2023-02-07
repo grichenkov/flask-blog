1f2169df0bd6bb5d430f31baa3333a081ae1b87c
a.mischenkov <a.mischenkov@vk.team>
2023-02-07 23:31:42 +0600

    Fixed db host

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 class Configuration(object):
     DEBUG = True
     sqlalchemy_track_modifications = False
-    SQLALCHEMY_DATABASE_URI = "mysql://root:1q2W3e4!@localhost/test1"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:1q2W3e4!@127.0.0.1:3307/test1"
     
