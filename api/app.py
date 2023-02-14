ea83929995d89716964000be66694f67eaa29147
Vladislav Grishchenkov <grichenkov.vlad@gmail.com>
2023-02-14 12:07:44 +0300

    закончил курс

--- api/app.py
+++ api/app.py
@@ -5,6 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
+
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
@@ -13,3 +15,4 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from models import *
+
